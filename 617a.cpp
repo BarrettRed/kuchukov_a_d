bdb706010d60d0bdc944108bcea5795643a23731
BarrettRed <113685615+BarrettRed@users.noreply.github.com>
2022-10-19 03:01:03 +0300

    .

--- 617a.cpp
+++ 617a.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
-using namespace std;
 
 int main() {
     int x;
-    cin >> x;
+    std::cin >> x;
     if (x > 5) {
         if (x % 5 == 0) {
             int k = x / 5;
-            cout << k;
+            std::cout << k;
         } else {
             int k = x / 5;
-            cout << 1 + k;
+            std::cout << 1 + k;
         }
     } else {
-        cout << 1;
+        std::cout << 1;
     }
 
 }
